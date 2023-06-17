8e8b355c5333e1ef9cadbb7a95052ab6afdd79d3
sharpman13 <sharpman2001@gmail.com>
2023-06-17 13:30:38 +0200

    feat: task5_list_files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { access, readdir } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const fileFolder = join(__dirname, 'files');
+
+const error = new Error('FS operation failed');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await access(fileFolder);
+    const files = await readdir(fileFolder);
+    console.table(files);
+  } catch {
+    throw error;
+  }
 };
 
 await list();
