0ddf7db785d8eceec0935bd23a650c44824cdd04
sharpman13 <sharpman2001@gmail.com>
2023-06-17 13:18:27 +0200

    feat: task4_delete_file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { access, unlink } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'files', 'fileToRemove.txt');
+
+const error = new Error('FS operation failed');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await access(file);
+    await unlink(file);
+  } catch {
+    throw error;
+  }
 };
 
 await remove();
