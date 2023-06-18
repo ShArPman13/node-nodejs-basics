ab87068e16b1b2de32c98db76bb26f7eb43372da
sharpman13 <sharpman2001@gmail.com>
2023-06-18 13:07:39 +0200

    feat: task8_cli_args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-  // Write your code here
+  const args = process.argv;
+  const resultArray = [];
+
+  for (let i = 2; i < args.length; i++) {
+    if (i % 2 === 0) {
+      resultArray.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+    }
+  }
+  console.log(resultArray.join(', '));
 };
 
 parseArgs();
