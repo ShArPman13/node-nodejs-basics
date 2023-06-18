47d4f32fda70b31f6f13cdf5edc73e6ac7c5a637
sharpman13 <sharpman2001@gmail.com>
2023-06-18 15:13:31 +0200

    feat: task10_streams_read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(file, 'utf-8');
+  readStream.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
