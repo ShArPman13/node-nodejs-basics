f0e12409674f5226d682a34269c48a03101f7a32
sharpman13 <sharpman2001@gmail.com>
2023-06-18 19:41:44 +0200

    feat: task14_zip_compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const file = join(__dirname, 'files', 'fileToCompress.txt');
+const fileStream = createReadStream(file);
+
+const compressedFile = join(__dirname, 'files', 'archive.gz');
+const compressedFileStream = createWriteStream(compressedFile);
+
 const compress = async () => {
-    // Write your code here 
+  fileStream.pipe(createGzip()).pipe(compressedFileStream);
 };
 
 await compress();
