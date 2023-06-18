52045dbaa8e472e4129658d303a0b0e8105d57f5
sharpman13 <sharpman2001@gmail.com>
2023-06-18 12:36:38 +0200

    feat: task7_cli_env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const env = process.env;
+  const entries = Object.entries(env);
+  const entriesRSS = entries
+    .filter((el) => el[0].includes(prefix))
+    .map((elem) => `${elem[0]}=${elem[1]}`);
+  console.log(entriesRSS.join('; '));
 };
 
 parseEnv();
